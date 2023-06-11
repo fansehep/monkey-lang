48bbcd8a9d5116ef42a941b14f4a1b2b738b0943
fansehep <yfan3763@gmail.com>
2023-06-11 19:47:59 +0800

    [repl]: fix repl errors
    Signed-off-by: fansehep <yfan3763@gmail.com>

--- repl/repl.go
+++ repl/repl.go
@@ -3,26 +3,58 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/fansehep/monkey-lang/executor"
 	"github.com/fansehep/monkey-lang/lexer"
-	"github.com/fansehep/monkey-lang/token"
+	"github.com/fansehep/monkey-lang/parser"
 	"io"
 )
 
 const PROMPT = ">> "
 
+// func Start(in io.Reader, out io.Writer) {
+// 	scaner := bufio.NewScanner(in)
+
+// 	for {
+// 		fmt.Fprintf(out, PROMPT)
+// 		scanned := scaner.Scan()
+// 		if !scanned {
+// 			return
+// 		}
+// 		line := scaner.Text()
+// 		l := lexer.New(line)
+// 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+// 			fmt.Fprintf(out, "%+v\n", tok)
+// 		}
+// 	}
+// }
+
 func Start(in io.Reader, out io.Writer) {
-	scaner := bufio.NewScanner(in)
+	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Fprintf(out, PROMPT)
-		scanned := scaner.Scan()
+		fmt.Fprint(out, PROMPT)
+		scanned := scanner.Scan()
 		if !scanned {
 			return
 		}
-		line := scaner.Text()
+		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		evaluated := executor.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
