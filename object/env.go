20a3229ba32a6005629c03c59558ded75960d918
fansehep <yfan3763@gmail.com>
2023-07-16 14:12:46 +0800

    chore: function env token
    Signed-off-by: fansehep <yfan3763@gmail.com>

---
+++ object/env.go
@@ -0,0 +1,34 @@
+package object
+
+
+type Environment struct {
+    store map[string]Object
+    outer *Environment
+}
+
+func NewEnvironment() *Environment {
+    return &Environment{
+        store: make(map[string]Object),
+        outer: nil,
+    }
+}
+
+func NewEnClosedEnvironment(oth *Environment) *Environment {
+    env := NewEnvironment()
+    env.outer = oth
+    return env
+}
+
+
+func (e *Environment) Get(name string) (Object, bool) {
+    obj, ok := e.store[name]
+    if !ok && e.outer != nil {
+        obj, ok = e.outer.Get(name)
+    }
+    return obj, ok
+}
+
+func (e *Environment) Set(name string, val Object) Object {
+    e.store[name] = val
+    return val
+}
